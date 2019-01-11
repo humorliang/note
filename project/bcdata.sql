76b968033e35aaede1f6daacd12158f36efdc11f
humorliang <humorliang@163.com>
2019-01-11 16:08:36 +0800

    primary key need next line

--- project/bcdata.sql
+++ project/bcdata.sql
@@ -4,7 +4,7 @@ USE `bg_cms`;
 
 /*用户表*/
 CREATE TABLE IF NOT EXISTS `bc_users`(
-    `user_id`  BIGINT(20)UNSIGNED PRIMARY KEY  AUTO_INCREMENT,
+    `user_id`  BIGINT(20)UNSIGNED AUTO_INCREMENT,
     `user_login` VARCHAR(60) NOT NULL COMMENT '用户名',
     `user_pass` VARCHAR(64) NOT NULL COMMENT '密码',
     `user_nicename` VARCHAR(50) DEFAULT '山猫' COMMENT '昵称',
@@ -12,31 +12,35 @@ CREATE TABLE IF NOT EXISTS `bc_users`(
     `user_registered` DATETIME DEFAULT now() COMMENT '注册时间',
     `user_activation_key` VARCHAR(60)  DEFAULT '0000' COMMENT '激活码',
     `user_status`   INT DEFAULT 0 COMMENT '用户状态',
+    PRIMARY KEY (`user_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /* 用户元素据表 */
 CREATE TABLE IF NOT EXISTS `bc_usermeta`(
-    `meta_id` BIGINT(20)UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `meta_id` BIGINT(20)UNSIGNED  AUTO_INCREMENT,
     `user_id` BIGINT(20)UNSIGNED DEFAULT 0 COMMENT '外键用户ID',
     `meta_key` VARCHAR(255),
     `meta_value` LONGTEXT,
+    PRIMARY KEY (`meta_id`),
     foreign key(`user_id`) references bc_users(`user_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /* 分类表 */
 CREATE TABLE IF NOT EXISTS `bc_terms`(
-    `term_id` BIGINT(20)UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `term_id` BIGINT(20)UNSIGNED  AUTO_INCREMENT,
     `name` varchar(200) NOT NULL  COMMENT '分类名',
     `term_group` INT DEFAULT 0 COMMENT '分类组',
+    PRIMARY KEY (`term_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /* 分类法表 */
 CREATE TABLE IF NOT EXISTS `bc_term_taxonomy`(
-    `term_taxonomy_id` BIGINT(20)UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `term_taxonomy_id` BIGINT(20)UNSIGNED  AUTO_INCREMENT,
     `term_id` BIGINT(20)UNSIGNED NOT NULL COMMENT '外键分类ID',
     `taxonomy` VARCHAR(32) NOT NULL COMMENT '分类法名',
     `description` VARCHAR(255) COMMENT '分类法描述',
     `parent_id` BIGINT(20)UNSIGNED DEFAULT 0 COMMENT '外键本身ID',
+    PRIMARY KEY (`term_taxonomy_id`),
     foreign key(`parent_id`) references bc_term_taxonomy(`term_taxonomy_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -49,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `bc_term_relationships`(
 
 /* 文章表 */
 CREATE TABLE IF NOT EXISTS `bc_posts`(
-    `post_id` BIGINT(20)UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `post_id` BIGINT(20)UNSIGNED  AUTO_INCREMENT,
     `post_author`  BIGINT(20)UNSIGNED DEFAULT 0 COMMENT '作者',
     `post_date`  DATETIME DEFAULT now() COMMENT '时间',
     `post_content` LONGTEXT COMMENT '内容',
@@ -59,22 +63,24 @@ CREATE TABLE IF NOT EXISTS `bc_posts`(
     `comment_status` VARCHAR(20) DEFAULT 'open' COMMENT '评论状态',
     `post_modified` DATETIME ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间', 
     `comment_count` INT COMMENT '评论数',
+    PRIMARY KEY (`post_id`),
     foreign key(`post_author`) references bc_users(`user_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /* 文章元数据表 */
-CREATE TABLE IF NOT NULL EXISTS `bc_postmeta`(
-    `meta_id` BIGINT(20)UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `bc_postmeta`(
+    `meta_id` BIGINT(20)UNSIGNED  AUTO_INCREMENT,
     `post_id` BIGINT(20)UNSIGNED DEFAULT 0 COMMENT '外键文章ID',
     `meta_key` VARCHAR(255),
     `meta_value` LONGTEXT,
+    PRIMARY KEY (`meta_id`),
     foreign key(`post_id`) references bc_posts(`post_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 /* 评论表 */
-CREATE TABLE IF NOT NULL EXISTS `bc_comments`(
-    `comment_id` BIGINT(20)UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT  EXISTS `bc_comments`(
+    `comment_id` BIGINT(20)UNSIGNED  AUTO_INCREMENT,
     `comment_post_id` BIGINT(20)UNSIGNED COMMENT '外键文章ID',
     `comment_author` TINYTEXT COMMENT '评论作者',
     `comment_author_email` VARCHAR(100) COMMENT '评论作者邮箱',
@@ -84,24 +90,26 @@ CREATE TABLE IF NOT NULL EXISTS `bc_comments`(
     `comment_approved` VARCHAR(20) COMMENT '是否被允许',
     `comment_parent` BIGINT(20)UNSIGNED DEFAULT 0 COMMENT '外键父评论ID',
     `user_id` BIGINT(20)UNSIGNED DEFAULT 0 COMMENT '外键用户ID',
+    PRIMARY KEY (`comment_id`),
     foreign key(`comment_post_id`) references bc_posts(`post_id`),
     foreign key(`comment_parent`) references bc_comments(`comment_id`),
     foreign key(`user_id`) references bc_users(`user_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /* 评论元数据表 */
-CREATE TABLE IF NOT NULL EXISTS `bc_commentmeta`(
-    `meta_id` BIGINT(20)UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT  EXISTS `bc_commentmeta`(
+    `meta_id` BIGINT(20)UNSIGNED  AUTO_INCREMENT,
     `comment_id` BIGINT(20)UNSIGNED DEFAULT 0 COMMENT '外键评论ID',
     `meta_key` VARCHAR(255),
     `meta_value` LONGTEXT,
+    PRIMARY KEY (`meta_id`),
     foreign key(`comment_id`) references bc_comments(`comment_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 /* 连接表 */
-CREATE TABLE IF NOT NULL EXISTS `bc_links`(
-    `link_id` BIGINT(20)UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT  EXISTS `bc_links`(
+    `link_id` BIGINT(20)UNSIGNED  AUTO_INCREMENT,
     `link_url` VARCHAR(255) COMMENT '链接URL',
     `link_name` VARCHAR(255) COMMENT '链接名称',
     `link_image` VARCHAR(255) COMMENT '链接图片',
@@ -110,6 +118,7 @@ CREATE TABLE IF NOT NULL EXISTS `bc_links`(
     `link_owner` BIGINT(20)UNSIGNED DEFAULT 1 COMMENT '添加者ID',
     `link_rating` INT DEFAULT 0 COMMENT '链接评分等级',
     `link_updated` DATETIME ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP  COMMENT '链接更新时间',
+    PRIMARY KEY (`link_id`),
     foreign key(`link_owner`) references bc_users(`user_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
